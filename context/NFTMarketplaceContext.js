54abc4a82f17bc4ee74878cd3dbe18e4abab27d6
sutarrohit <rohitsutar082@gmail.com>
2023-02-07 17:18:52 +0530

    Modal Update

--- context/NFTMarketplaceContext.js
+++ context/NFTMarketplaceContext.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import web3Modal from "web3Modal";
+import Web3Modal from "web3Modal";
 import { Contract, ContractFactory, ethers } from "ethers";
 import Router from "next/router";
 import { useRouter } from "next/router";
@@ -37,8 +37,8 @@ const fetchContract = (signerorProvider) =>
 //----CONNECTING WITH SMART CONTRACT
 const connectingWithSmartContract = async () => {
   try {
-    const Web3Modal = new web3Modal();
-    const connection = await Web3Modal.connect();
+    const getWeb3Modal = new Web3Modal();
+    const connection = await getWeb3Modal.connect();
     const provider = new ethers.providers.Web3Provider(connection);
     const signer = provider.getSigner();
     const contract = fetchContract(signer);
